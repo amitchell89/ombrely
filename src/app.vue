951a19d33fbdc13ca75b67207db718f6378f4543
Aaron Mitchell <amitchell@cogolabs.com>
2016-12-28 17:54:34 -0500

    adding gradient generator

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="message">{{ msg }}</div>
+  <div class="container" v-bind:style="gradient">
+    <div class="message">{{ msg }}</div>
+    <button v-on:click='generateGradient' class="btn">Hit Me</button>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      msg: 'Hello from vue-loader!'
+      msg: 'Welcome to Ombrely',
+      hex1: 'eeeeee',
+      hex2: 'aaaaaa'
+    }
+  },
+  computed: {
+    gradient: function () {
+      return {
+        background: '-webkit-linear-gradient(left, #' + this.hex1 + ', #' + this.hex2 +')',
+        background: '-moz-linear-gradient(left, #' + this.hex1 + ', #' + this.hex2 +')',
+        background: 'linear-gradient(to right, #' + this.hex1 + ', #' + this.hex2 +')'
+      }
+    }
+  },
+  methods: {
+    generateGradient: function() {
+      this.hex1 = '';
+      this.hex2 = '';
+      var charset = "abcdef0123456789";
+
+      for (var i = 0; i < 6; i++) {
+        this.hex1 += charset.charAt(Math.floor(Math.random() * charset.length));
+        this.hex2 += charset.charAt(Math.floor(Math.random() * charset.length));
+      }
     }
   }
 }
@@ -14,7 +40,9 @@ export default {
 
 <style>
 .message {
-  background: #aaa;
   color: white;
+  padding-bottom: 2rem;
 }
 </style>
+
+
